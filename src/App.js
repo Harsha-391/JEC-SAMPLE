ce348694b202ad9ac77cdd902b821fdfe3e18348
Harsha-391 <143133227+Harsha-391@users.noreply.github.com>
2025-11-13 12:44:58 +0530

    update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Admissions from './pages/Admissions';
-import Placements from './pages/Placements'; // <-- 1. IMPORT YOUR NEW PAGE
+import Placements from './pages/Placements';
+import About from './pages/About'; // <-- 1. IMPORT YOUR NEW PAGE
 
 function App() {
   return (
@@ -17,7 +18,8 @@ function App() {
           
           <Route index element={<Home />} /> 
           <Route path="admissions" element={<Admissions />} /> 
-          <Route path="placements" element={<Placements />} /> {/* <-- 2. ADD THE ROUTE */}
+          <Route path="placements" element={<Placements />} />
+          <Route path="about" element={<About />} /> {/* <-- 2. ADD THE ROUTE */}
 
         </Route>
       </Routes>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 // src/components/Header.js
 import React from 'react';
+import { Link } from 'react-router-dom'; // <-- 1. IMPORT LINK
 
 function Header() {
   return (
@@ -10,7 +11,10 @@ function Header() {
           <a href="#alumni">Alumni</a>
           <a href="#parents">Parents</a>
           <a href="#contact">Contact Us</a>
-          <a href="#about">About Us</a>
+
+          {/* 2. CHANGE THIS LINE: */}
+          <Link to="/about">About Us</Link>
+
           <button className="apply-btn">Apply Now</button>
         </nav>
       </div>
