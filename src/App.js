58ee206ae141920dc7c2ee8e081ecf6320e696bc
Harsha-391 <143133227+Harsha-391@users.noreply.github.com>
2025-11-13 12:08:54 +0530

    Add Placements page with testimonials

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
 // src/App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'; // Your main stylesheet
+import './App.css';
 
 // Import your Layout and Page components
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Admissions from './pages/Admissions';
-// Import other pages as you create them...
+import Placements from './pages/Placements'; // <-- 1. IMPORT YOUR NEW PAGE
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* This Route uses your Layout. All other pages go inside it. */}
         <Route path="/" element={<Layout />}>
           
-          {/* 'index' means this is the default page for "/" */}
           <Route index element={<Home />} /> 
-          
-          {/* This is your new Admissions page */}
           <Route path="admissions" element={<Admissions />} /> 
-
-          {/* You can add more pages here */}
-          {/* <Route path="about" element={<About />} /> */}
-          {/* <Route path="contact" element={<Contact />} /> */}
+          <Route path="placements" element={<Placements />} /> {/* <-- 2. ADD THE ROUTE */}
 
         </Route>
       </Routes>
