05d6d76514ad70ab61b8ee09c29298cf28948ce2
Harsha-391 <143133227+Harsha-391@users.noreply.github.com>
2025-11-13 16:59:05 +0530

    header subheader

--- src/components/Subheader.js
+++ src/components/Subheader.js
@@ -5,24 +5,30 @@ import { Link } from 'react-router-dom';
 function Subheader() {
   return (
     <div className="subheader">
-      <div className="subheader-content">
+      <div className="subheader-content max-width-container">
         
+        {/* Logo on the Left */}
         <Link to="/" className="subheader-logo">
-          <img src="/images/logo.png" alt="Company Logo" />
-          <span>Jaipur Engineering College</span>
+          <img src="/images/logo.png" alt="Jaipur Engineering College Logo" />
+          <div className="logo-text">
+            <span>JAIPUR ENGINEERING</span>
+            <span>COLLEGE</span>
+          </div>
         </Link>
 
-        <div className="subheader-nav">
-          <a href="/#academics">Academics</a> 
-          <Link to="/admissions">Admissions</Link>
-          <a href="/#outcomes">Student Outcomes</a>
-          
-          {/* CHANGE THIS LINE: */}
-          <Link to="/placements">Placements</Link>
-
-          <a href="/#campus">Campus Life</a>
-          <a href="/#research">Research</a>
-        </div>
+        {/* Main Navigation on the Right */}
+        <nav className="subheader-nav">
+          <Link to="/">Home</Link>
+          <a href="#!">JEC</a>
+          <Link to="/admissions">Admission</Link>
+          <Link to="/placements">Placement</Link>
+          <a href="#!">Departments</a>
+          <a href="#!">Infrastructure</a>
+          <a href="#!">Campus Life</a>
+          <a href="#!">Our Society</a>
+          <Link to="/contact">Contact Us</Link>
+          <Link to="/about">About Us</Link>
+        </nav>
 
       </div>
     </div>
