5cfe2560c312283c481fd86c33096aaca73eb3de
Harsha-391 <143133227+Harsha-391@users.noreply.github.com>
2025-11-13 16:42:57 +0530

    Implement new two-part header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,35 @@
 // src/components/Header.js
 import React from 'react';
-import { Link } from 'react-router-dom'; // <-- 1. IMPORT LINK
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header>
-      <div className="header-content">
-        <nav>
-          <a href="#students">Students</a>
-          <a href="#alumni">Alumni</a>
-          <a href="#parents">Parents</a>
-
-          {/* 2. CHANGE THIS LINE: */}
-          <Link to="/Contact">Contact Us</Link>
-
-          <Link to="/about">About Us</Link>
-          <button className="apply-btn">Apply Now</button>
+      <div className="header-content max-width-container">
+        {/* Contact Info on the Left */}
+        <div className="header-contact">
+          <a href="tel:+918875071333">
+            <i className="fas fa-phone"></i> +91-8875071333 (30 lines)
+          </a>
+          <a href="mailto:admission@jeckukas.org.in">
+            <i className="fas fa-envelope"></i> admission@jeckukas.org.in
+          </a>
+        </div>
+        
+        {/* Links on the Right */}
+        <nav className="header-nav">
+          <a href="#!">
+            <i className="fas fa-edit"></i> Admission Enquiry Open 2025
+          </a>
+          <Link to="/admissions">
+            <i className="fas fa-user-graduate"></i> Apply for Admission
+          </Link>
+          <a href="#!">
+            <i className="fas fa-map-marked-alt"></i> JEC 360 Virtual Campus Tour
+          </a>
+          <a href="#!">
+            <i className="fas fa-bullhorn"></i> Grievance Redressal Form
+          </a>
         </nav>
       </div>
     </header>
