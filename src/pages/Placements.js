af8006835cf6a2c40206936fcd96bb1aa22eda5d
Harsha-391 <143133227+Harsha-391@users.noreply.github.com>
2025-11-13 12:21:49 +0530

    gloryy

--- src/pages/Placements.js
+++ src/pages/Placements.js
@@ -6,7 +6,7 @@ function Placements() {
     <div className="placements-page">
       {/* 1. The Banner Image */}
       <img
-        src="/images/placement-glory.jpg"
+        src="/images/placement-Glory.jpg"
         alt="JEC Placements Glory"
         className="placements-banner"
       />
