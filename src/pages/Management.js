473571a456b547976395e9372c9e21b3b45cebc6
Harsha-391 <143133227+Harsha-391@users.noreply.github.com>
2025-11-17 13:52:29 +0530

    lakshay image

--- src/pages/Management.js
+++ src/pages/Management.js
@@ -28,7 +28,7 @@ function Management() {
                 Example: src="/images/lc-saraogi.jpg" 
               */}
               <img 
-                src="C:\Users\Harsh\Downloads\jec react\public\images\1574843524 Lalit saraogi.png"
+                src="C:\Users\Harsh\Downloads\jec react\public\images\1574843524_Lalit saraogi.png"
                 alt="Shri L.C. Saraogi" 
                 className="leader-img" 
               />
@@ -154,7 +154,7 @@ function Management() {
                 Example: src="/images/lakshya-saraogi.jpg" 
               */}
               <img 
-                src="../public/images/lakshaysaraogi.jpg" 
+                src="C:\Users\Harsh\Downloads\jec react\public\images\lakshaysaraogi.jpg" 
                 alt="Mr. Lakshya Saraogi" 
                 className="leader-img" 
               />
