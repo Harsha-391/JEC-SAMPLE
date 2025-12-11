8fdc0049deb5a223c310f7829dabc6894ece7607
Harsha-391 <143133227+Harsha-391@users.noreply.github.com>
2025-12-11 17:48:39 +0530

    zdfgh

--- src/index.js
+++ src/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const container = document.getElementById('root');
-
-// If pre-rendered HTML exists, use hydrateRoot. Otherwise use createRoot.
-if (container.hasChildNodes()) {
-  hydrateRoot(container, 
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  const root = createRoot(container);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
 
 reportWebVitals();
